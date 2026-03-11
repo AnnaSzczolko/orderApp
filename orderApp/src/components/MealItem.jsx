63fbeb8caf4b86b4247c55df9904a7ec0f03694f
Ania <anna.szczolko@gmail.com>
2026-03-11 15:14:37 +0100

    changed path to image in MealItem

--- orderApp/src/components/MealItem.jsx
+++ orderApp/src/components/MealItem.jsx
@@ -8,7 +8,7 @@ const API_URL = 'https://orderapp-backend-tpks.onrender.com'
 
 function MealItem({ item }) {
 	const { id, name, price, image, description, orders } = item
-	const img = `{API_URL}/${image}`
+	const img = `{API_URL}${image}`
 
 	const dispatch = useDispatch()
 
